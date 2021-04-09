5a0fb35770c5ec1a719b3cdb9e9e65e7baa16f6c
shubhami <singale8888@gmail.com>
2021-04-09 09:49:44 +0530

    first push maven

---
+++ src/main/java/com/example/MainServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/get")
+	public String m1() {
+		
+		
+		return "success";
+	}
+	
+
+}
